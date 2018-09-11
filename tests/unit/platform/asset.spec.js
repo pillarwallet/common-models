4514e20bf41b013385deb435b40b8037c8466881
Vinicius Raszl <vini.ra@outlook.com>
2018-09-11 16:58:36 +0100

    Bump version Add unit test to Asset Model

--- tests/unit/platform/asset.spec.js
+++ tests/unit/platform/asset.spec.js
@@ -1,10 +1,36 @@
-const models = require('../../../index');
+const AssetModel = require('../../../lib/platform/asset');
 
-//TODO Model Asset is allowing to create empty object.
+// TODO Model Asset is allowing to create empty object.
 describe('Asset Schema Validation', () => {
   let assetObject;
   let Asset;
   let error;
+
+  it('should be invalid when "decimals" field is not Number.', () => {
+    assetObject = {
+      decimals: 'notNumberValue',
+    };
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(Asset.validateSync).toThrow();
+    expect(error.errors.decimals.message).toEqual(
+      'Cast to Number failed for value "notNumberValue" at path "decimals"',
+    );
+  });
+
+  it('should be invalid when "isDefault" field is not Boolean.', () => {
+    assetObject = {
+      isDefault: 'WrongType',
+    };
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(Asset.validateSync).toThrow();
+    expect(error.errors.isDefault.message).toEqual(
+      'Cast to Boolean failed for value "WrongType" at path "isDefault"',
+    );
+  });
 
   it('should create object successfully', () => {
     assetObject = {
@@ -12,7 +38,7 @@ describe('Asset Schema Validation', () => {
       name: 'MyCoin',
       symbol: 'MCN',
     };
-    Asset = new models.platform.Asset(assetObject);
+    Asset = new AssetModel(assetObject);
     error = Asset.validateSync();
 
     expect(error).toBe(undefined);
