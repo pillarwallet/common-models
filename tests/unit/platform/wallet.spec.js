6bb7a93f1ed7030d9cd43a6fb88f5884e63111e3
Vinicius Raszl <vini.ra@outlook.com>
2018-09-11 14:39:02 +0100

    Wallet-Model (#2)
    Add Wallet Model and Schema Constraints
    Add Wallet index file
    Add unit test
    Add mongoose and uuid lib
    Add unit test for schemaCreator.js
    Add test scripts in package.json file
    Add unit test to wallet.spec in order to check unique fields

---
+++ tests/unit/platform/wallet.spec.js
@@ -0,0 +1,119 @@
+const schemaCreator = require('../../../utilities/schemaCreator.js');
+
+jest.spyOn(schemaCreator, 'createSchema');
+const models = require('../../../index');
+
+describe('Wallet Schema Validation', () => {
+  let walletObject;
+  let Wallet;
+  let error;
+  // function to get mocker first call
+  const getMockFirstCall = call => call.mock.calls[0][0];
+
+  afterAll(() => {
+    schemaCreator.createSchema.mockRestore();
+  });
+
+  it('should be invalid if try to create empty object', () => {
+    walletObject = {};
+    Wallet = new models.platform.Wallet(walletObject);
+
+    expect(Wallet.validateSync).toThrow();
+  });
+
+  it('should be invalid if try to create object without userId field', () => {
+    walletObject = {
+      publicKey: 'myKey',
+      ethAddress: 'myEthAddress',
+      fcmToken: 'fcmId',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+
+    expect(Wallet.validateSync).toThrow();
+    expect(error.errors.userId.message).toEqual('Path `userId` is required.');
+  });
+
+  it('should be invalid if try to create object without publicKey field', () => {
+    walletObject = {
+      userId: 'myUserId',
+      ethAddress: 'myEthAddress',
+      fcmToken: 'fcmId',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+
+    expect(Wallet.validateSync).toThrow();
+    expect(error.errors.publicKey.message).toEqual(
+      'Path `publicKey` is required.',
+    );
+  });
+
+  it('should be invalid if try to create object without ethAddress field', () => {
+    walletObject = {
+      userId: 'myUserId',
+      publicKey: 'myKey',
+      fcmToken: 'fcmId',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+
+    expect(Wallet.validateSync).toThrow();
+    expect(error.errors.ethAddress.message).toEqual(
+      'Path `ethAddress` is required.',
+    );
+  });
+
+  it('should be invalid if try to create object without fcmToken field', () => {
+    walletObject = {
+      userId: 'myUserId',
+      publicKey: 'myKey',
+      ethAddress: 'myEthAddress',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+
+    expect(Wallet.validateSync).toThrow();
+    expect(error.errors.fcmToken.message).toEqual(
+      'Path `fcmToken` is required.',
+    );
+  });
+
+  it('should be invalid when bcxRegistered field is not Boolean.', () => {
+    walletObject = {
+      userId: 'myUserId',
+      publicKey: 'myKey',
+      ethAddress: 'myEthAddress',
+      fcmToken: 'fcmId',
+      bcxRegistered: 'WrongType',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+
+    expect(Wallet.validateSync).toThrow();
+    expect(error.errors.bcxRegistered.message).toEqual(
+      'Cast to Boolean failed for value "WrongType" at path "bcxRegistered"',
+    );
+  });
+
+  it('creates Schema with unique properties', () => {
+    const firstCall = getMockFirstCall(schemaCreator.createSchema);
+
+    expect(firstCall.publicKey).toHaveProperty('unique', true);
+    expect(firstCall.userId).toHaveProperty('unique', true);
+    expect(firstCall.ethAddress).toHaveProperty('unique', true);
+  });
+
+  it('should create object successfully', () => {
+    walletObject = {
+      userId: 'myUserId',
+      publicKey: 'myKey',
+      ethAddress: 'myEthAddress',
+      fcmToken: 'fcmId',
+    };
+    Wallet = new models.platform.Wallet(walletObject);
+    error = Wallet.validateSync();
+    expect(error).toBe(undefined);
+    expect(Wallet).toMatchObject(walletObject);
+  });
+});
