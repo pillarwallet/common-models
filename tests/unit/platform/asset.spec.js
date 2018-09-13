7c6bf991a54dc6b3d1b5ccaec6339e8b1eb5a689
Vinicius Raszl <vini.ra@outlook.com>
2018-09-13 10:32:19 +0100

    Asset model (#6)
    Add Asset Model
    Add test to verify unique field in model schema
    Import only the model that is being tested on spec files
    Export Asset Module
    Add unit Test
    Bump version
    Add unit test to Asset Model
    Review PR:
    Add constraints to address , decimals, name and symbol fields of asset model
    Check it on unit tests

---
+++ tests/unit/platform/asset.spec.js
@@ -0,0 +1,88 @@
+const schemaCreator = require('../../../utilities/schemaCreator.js');
+
+jest.spyOn(schemaCreator, 'createSchema');
+const AssetModel = require('../../../lib/platform/asset');
+
+describe('Asset Schema Validation', () => {
+  let assetObject;
+  let Asset;
+  let error;
+
+  afterAll(() => {
+    schemaCreator.createSchema.mockRestore();
+  });
+
+  const getMockFirstCall = call => call.mock.calls[0][0];
+
+  it('should be invalid if try to create empty object', () => {
+    assetObject = {};
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(Asset.validateSync).toThrow();
+    expect(error.errors.address.message).toEqual('Path `address` is required.');
+    expect(error.errors.decimals.message).toEqual(
+      'Path `decimals` is required.',
+    );
+    expect(error.errors.name.message).toEqual('Path `name` is required.');
+    expect(error.errors.symbol.message).toEqual('Path `symbol` is required.');
+  });
+
+  it('should be invalid when "decimals" field is not Number.', () => {
+    assetObject = {
+      decimals: 'notNumberValue',
+    };
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(Asset.validateSync).toThrow();
+    expect(error.errors.decimals.message).toEqual(
+      'Cast to Number failed for value "notNumberValue" at path "decimals"',
+    );
+  });
+
+  it('should be invalid when "isDefault" field is not Boolean.', () => {
+    assetObject = {
+      isDefault: 'WrongType',
+    };
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(Asset.validateSync).toThrow();
+    expect(error.errors.isDefault.message).toEqual(
+      'Cast to Boolean failed for value "WrongType" at path "isDefault"',
+    );
+  });
+
+  it('creates Schema with unique properties', () => {
+    const firstCall = getMockFirstCall(schemaCreator.createSchema);
+
+    expect(firstCall.address).toHaveProperty('unique', true);
+    expect(firstCall.name).toHaveProperty('unique', true);
+    expect(firstCall.symbol).toHaveProperty('unique', true);
+  });
+
+  it('should create object successfully', () => {
+    assetObject = {
+      address: '0xaabbc12322321b',
+      decimals: 12,
+      description: 'One Simple Token that aims to be the best ever',
+      name: 'BananaCoin',
+      symbol: 'BCN',
+      wallpaperUrl: 'http://banana.com/bg',
+      iconUrl: '/image.png',
+      email: 'bananacoin@jungle.com',
+      telegram: 'myTelegram',
+      twitter: 'myTwitter',
+      website: 'http://banana.com',
+      whitepaper: 'Whitepaper',
+      isDefault: true,
+      isDefaultToken: true,
+    };
+    Asset = new AssetModel(assetObject);
+    error = Asset.validateSync();
+
+    expect(error).toBe(undefined);
+    expect(Asset).toMatchObject(assetObject);
+  });
+});
